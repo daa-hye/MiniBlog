865c83dcc7c533dc91ab9750b6a27913882c19b1
dahye <bbngsk@gmail.com>
2023-11-12 19:49:52 +0900

    Initial Commit

---
+++ MiniBlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniBlog
+//
+//  Created by 박다혜 on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
